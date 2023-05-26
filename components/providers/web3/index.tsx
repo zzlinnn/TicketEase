35fa8e445a5b2065fb896d345db0dfc87af63057
zhilin <zhilin_yap@yahoo.com>
2023-05-27 00:19:41 +0900

    Starting writing contracts

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent, createContext, useContext, useState, useEffect } from "react";
+import { Web3State, createDefaultState } from "./utils";
+import { ethers } from "ethers";
+
+
+const Web3Context = createContext<Web3State>(createDefaultState());
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const Web3Provider: FunctionComponent<Props> = ({children}) => {
+    const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState())
+
+    useEffect(() =>{
+        function initWeb3() {
+            const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+           setWeb3Api({
+            ethereum: window.ethereum,
+            provider,
+            contract: null,
+            isLoading: false
+           })
+        }
+
+        initWeb3();
+    }, [])
+    return (
+        <Web3Context.Provider value={web3Api}>
+            {children}
+        </Web3Context.Provider>
+
+    )
+}
+
+export function useWeb3() {
+    return useContext(Web3Context); 
+}
+
+export default Web3Provider; 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { Web3Provider } from '@providers'
 import type { AppProps } from 'next/app'
 
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
+
+    </>
+  )
 }
